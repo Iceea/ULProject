0baeeee09a7f9214d2a804e0baeff1f7f5227ae0
Iceea <fabiom456@gmail.com>
2017-09-13 02:50:57 -0300

    Primeiro passo
    O código está uma bagunça, mas este é o 1° modelo que faço para o site
    
    SUGESTÕES..........

---
+++ lista.js
@@ -0,0 +1,47 @@
+	selecionar(0);
+	var fundo = document.getElementById("fundo");
+	var sobre = document.getElementById("sobre");
+	fundo.style.height = window.innerHeight;
+	sobre.style.top = window.innerHeight;
+window.onresize = function(){
+	var fundo = document.getElementById("fundo");
+	fundo.style.height = window.innerHeight;
+}
+
+var last_known_scroll_position = 0;
+var ticking = false;
+
+document.getElementById('bthome').addEventListener('click', function(){window.scrollTo(0, 0)});
+document.getElementById('btsobre').addEventListener('click', function(){window.scrollTo(0, window.innerHeight)});
+
+function selecionar(x){
+	var selected;
+	var vetor = ['bthome', 'btsobre'];
+	vetor.forEach( function(element, index) {
+		selected = document.getElementById(vetor[index]);
+		selected.style.color = 'white';
+		selected.style.fontSize = '1em';
+	});
+	selected = document.getElementById(vetor[x]);
+	selected.style.color = 'red';
+	selected.style.fontSize = '1.3em';
+}
+
+function doSomething(scroll_pos) {
+	if(window.innerHeight - window.scrollY < 0) sobre.style.top = 0;
+	else sobre.style.top = window.innerHeight - window.scrollY;
+	fundo.style.filter = 'blur('+10*window.scrollY/window.innerHeight+'px)';
+	if(window.scrollY < window.innerHeight * 0.5) selecionar(0);
+	else if(window.scrollY < window.innerHeight * 1.5) selecionar(1);
+}
+
+window.addEventListener('scroll', function(e) {
+  last_known_scroll_position = window.scrollY;
+  if (!ticking) {
+    window.requestAnimationFrame(function() {
+      doSomething(last_known_scroll_position);
+      ticking = false;
+    });
+  }
+  ticking = true;
+});
